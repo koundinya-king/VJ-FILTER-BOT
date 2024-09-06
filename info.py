558350b9c58af1323172f6dd477c731a930a6acc
koundinya-king <koundinyabots@gmail.com>
2024-09-06 21:22:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -69,7 +69,7 @@ OWNER_USERNAME = environ.get('OWNER_USERNAME', 'ABa2zm') # owner username withou
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/SSK_MOVIES')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/SSK_MOVIES')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_Linkl')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'atozmoviesrequest') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/+Y9aDbpFtZ70zOGFl') # Support Chat Link Without https:// or @
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
